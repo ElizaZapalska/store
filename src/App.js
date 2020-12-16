79a20bbcbe3610ba60dbef184c0224357194dc5f
Eliza Zapalska <zapalska.eliza@gmail.com>
2020-12-16 12:17:00 +0100

    add Hero component and image

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from "react";
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Navbar from "./Components/Navbar";
+import Home from "./Components/pages/Home";
 
 
 function App() {
@@ -9,6 +10,11 @@ function App() {
   <>
     <Router>
       <Navbar />
+      <Switch>
+        <Route path={"/"} exact component={Home} />
+        {/*<Route path={'/products'} component={Products} />
+        <Route path={'/sign-up'} component={SignUp} />*/}
+      </Switch>
     </Router>
     </>
   )
---
+++ src/Components/Hero.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../App.css';
+import './Hero.css';
+
+function Hero() {
+    return (
+        <div className={'hero'}>
+
+            <div className={'title'}>
+                <h1>Eliza's Store</h1>
+            </div>
+
+            <div className={'image'}>
+                <img src={"../images/herophoto.png"} alt="Smiley face"/>
+
+            </div>
+            </div>
+    )
+}
+
+export default Hero
